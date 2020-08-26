0d26d9352cbfa408a996cf8a5817b7b26f9a4192
aumhaa <aumhaa@gmail.com>
2020-08-26 15:56:51 -0700

    _base, promise   :   added some code to _base to accommodate building frozen projects from Max's Project Manager (which flattens the aumhaa folder structure, making the usual method break). Added Promise.each() method to aumhaa_promise.

---
+++ aumhaa_filenames.js
@@ -0,0 +1,26 @@
+var filenames = [
+  'aumhaa_mod',
+  'aumhaa_bindable',
+  'aumhaa_midi_io_mmodule',
+  'aumhaa_notifier_consts',
+  'aumhaa_node_script_proxy',
+  'aumhaa_event_emitter',
+  'aumhaa_floating_window',
+  'aumhaa_button_class',
+  'aumhaa_util',
+  'aumhaa_LiveAPI_util',
+  'aumhaa_chooser',
+  'aumhaa_control_class',
+  'aumhaa_control_registry',
+  'aumhaa_control_surface',
+  'aumhaa_dict_module',
+  'aumhaa_global_proxy',
+  'aumhaa_grid_class',
+  'aumhaa_multi_notifier',
+  'aumhaa_notification_display',
+  'aumhaa_promise',
+  'aumhaa_scales',
+  'aumhaa_task_server'
+]
+
+exports.filenames = filenames;
